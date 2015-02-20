2e815895399610872b8392d90929b580f842cfe4
JabbaDuhHutt <michael.pickett72@gmail.com>
2015-02-20 13:47:25 -0500

    Prepared for State Machine
    I think I have all the parts in here, but you probably want to double
    check. The pins need to actually be set to numbers, but everything looks
    fine. Might need to change the states a bit depends on how we tackle the
    second reactor but I think we could just have variable that gets flagged
    and changes which reactor.

--- MCP_RBE2001.ino
+++ MCP_RBE2001.ino
@@ -1,15 +1,57 @@
+#include <Servo.h>
 
+#define leftDrivePin;
+#define rightDrivePin;
+#define fourBarPin;
+#define rackMotorPin;
+#define grabberServoPin;
+#define limitPin;
+#define lineSensePin1;
+#define lineSensePin2;
+#define lineSensePin3;
+#define lineSensePin4;
+#define potPin;
 
+Servo leftDrive;
+Servo rightDrive;
+Servo fourBarMotor;
+Servo rackMotor;
+Servo grabberServo;
+
+enum State{
+	findReactor;
+	grabSpent;
+	findDisposal;
+	placeSpent;
+	findSupply;
+	grabSupply;
+	returnToReactor;
+	placeSupply;
+}
 void setup(){
+	leftDrive.attach(leftDrivePin, 1000, 2000);
+	rightDrive.attach(rightDrivePin, 1000, 2000);
+	fourBarMotor.attach(fourBarPin, 1000, 2000);
+	rackMotor.attach(rackMotorPin, 1000, 2000);
+	grabberServo.attach(grabberServoPin);
 }
 
 void loop(){
   
-  robot.findReactor();
-  robot.grabRod();
-  robot.findDisposal();
-  robot.placeRod();
-  robot.findReactor();
-  
-  
+ 	main();
+
+}
+
+void main(){
+	switch () {
+	    case :
+	      // do something
+	      break;
+	    case :
+	      // do something
+	      break;
+	    default:
+	      // do something
+	}
+
 }
