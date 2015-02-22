bdfa19be493235fb85a4e38176c9a14380a30425
JabbaDuhHutt <michael.pickett72@gmail.com>
2015-02-22 00:28:58 -0500

    Added FindReactor Method
    -also added a forward and line hit method

--- MCP_RBE2001.ino
+++ MCP_RBE2001.ino
@@ -1,16 +1,26 @@
 #include <Servo.h>
+//all pins are temporary and need changing
+#define leftDrivePin 24
+#define rightDrivePin 23
+#define fourBarPin 25
+#define rackMotorPin 26
+#define grabberServoPin 27
+#define limitPin A0
+#define lineSensePin1 A1
+#define lineSensePin2 A2
+#define lineSensePin3 A3
+#define lineSensePin4 A4
+#define potPin A5
 
-#define leftDrivePin;
-#define rightDrivePin;
-#define fourBarPin;
-#define rackMotorPin;
-#define grabberServoPin;
-#define limitPin;
-#define lineSensePin1;
-#define lineSensePin2;
-#define lineSensePin3;
-#define lineSensePin4;
-#define potPin;
+#define leftFWD 180
+#define rightFWD 0
+#define leftBWD 0
+#define rightBWD 180
+
+char reactor; //reactor type
+int lineCount; //counts amount of lines
+int lineFlag; //flage when line detected
+
 
 Servo leftDrive;
 Servo rightDrive;
@@ -30,32 +40,84 @@ enum State{
 };
 
 State state;
-void setup(){
-	leftDrive.attach(leftDrivePin, 1000, 2000);
-	rightDrive.attach(rightDrivePin, 1000, 2000);
-	fourBarMotor.attach(fourBarPin, 1000, 2000);
-	rackMotor.attach(rackMotorPin, 1000, 2000);
-	grabberServo.attach(grabberServoPin);
 
+void setup(){
+	leftDrive.attach(leftDrivePin,1000,2000);
+	rightDrive.attach(rightDrivePin,1000,2000);
+	fourBarMotor.attach(fourBarPin,1000,2000);
+	rackMotor.attach(rackMotorPin,1000,2000);
+	grabberServo.attach(grabberServoPin);
+	pinMode(lineSensePin1, INPUT);
+	pinMode(lineSensePin2, INPUT);
+	pinMode(lineSensePin3, INPUT);
+	pinMode(lineSensePin4, INPUT);
+	pinMode(potPin, INPUT);
+	pinMode(limitPin, INPUT);
 	state = findReactor;
+	reactor = 'A'; //reactor type
+	lineCount = 0; //counts the amount of lines
+	lineFlag = 0; //flag for when line detected 
 }
 
 void loop(){
   
- 	main();
+ 	stateMachine();
 
 }
-
-void main(){
+//state machine that runs between states of 
+void stateMachine(){
 	switch (state) {
-	    case :
+	    case findReactor: //finds a reactor
+	      if(reactor == 'A'){ //for reactor A
+	      	forward();	
+	      	if(lineHit(1)){ //changes depends on where we start
+	      		if(analogRead(limitPin) == LOW){
+	      		state = grabSpent;
+	      		}
+	      	}
+	      }
+	      if(reactor == 'B'){ //for reactor B
+	      	if(lineHit(4)){ //might need to change depending on where it turns around
+	      		if(analogRead(limitPin) == LOW){
+	      		state = grabSpent;
+	      		}
+	      	}
+	      }
+	      break;
+	    case grabSpent:
 	      // do something
 	      break;
-	    case :
-	      // do something
-	      break;
-	    default:
-	      // do something
+	    case findDisposal:
+	    	break;
+	    case placeSpent:
+	    	break;
+	    case findSupply:
+	    	break;
+	    case grabSupply:
+	    	break;
+	    case returnToReactor:
+	    	break;
+	    case placeSupply:
+	    	break;
 	}
 
+}
+//returns boolean if a certain amount of lines have been hit
+boolean lineHit(int x){
+	if((lineFlag == 0) && (analogRead(lineSensePin4 == HIGH))){
+		lineCount++;
+		lineFlag = 1; 
+	}else if((lineFlag == 1) && (analogRead(lineSensePin4 == LOW))){
+		lineFlag = 0;			
+	}				
+	if(lineCount == x){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void forward(){
+	leftDrive.write(leftFWD);
+	rightDrive.write(rightFWD);
 }
